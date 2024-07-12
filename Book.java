2673289bfb8b53813615d3e7ca881eecdb22161e
Abdelhadi HADDADI <76612137+abdelhadi-haddadi@users.noreply.github.com>
2024-07-12 18:57:14 +0100

    Create Book.java

---
+++ Book.java
@@ -0,0 +1,22 @@
+import  java.util.Scanner ;
+public class Book {
+
+
+    public static  void main(String[] args){
+      
+        Scanner sc = new Scanner(System.in);
+        String name = "abdelahdi haddadi";
+      
+        float a,b ;
+      
+        System.out.println("Entrer un nombre : ");
+        a = sc.nextFloat();
+      
+        System.out.println("Entrer un nombre : ");
+        b = sc.nextFloat();
+
+        System.out.println("my number is : " + (a + b));
+        System.out.println("my num is " + a + " " + b);
+      
+    }
+}
