d006327f03d0592b3bc5329e5757099f4ca807e0
Abdelhadi HADDADI <76612137+abdelhadi-haddadi@users.noreply.github.com>
2024-07-12 23:10:27 +0100

    Create arrays.java

---
+++ arrays.java
@@ -0,0 +1,17 @@
+public class arrays {
+    public static void main(String[] args){
+        int[] myArray = new int[4];
+
+        myArray[0] = 18 ;
+        myArray[1] = 22 ;
+        myArray[2] = 24 ;
+        myArray[3] =  4 ;
+        for ( int i = 0 ; i < myArray.length ; i++ ){
+            System.out.println(myArray[i]);
+        }
+
+
+        String[] array = {"abdelhadi" , "ali" ,"hamid"} ;
+        System.out.println(array[2]);
+    }
+}
