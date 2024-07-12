dcb0398a7c3e6a1fa673eeb796d075e46e5f7cec
Abdelhadi HADDADI <76612137+abdelhadi-haddadi@users.noreply.github.com>
2024-07-12 19:23:03 +0100

    Create Operator.java

---
+++ Operator.java
@@ -0,0 +1,35 @@
+public class Operator {
+    public static  void main(String[] args){
+        // Arithmetic Operators
+        int a , b ;
+        a = 20 ;
+        b = 200 ;
+        // Addition +
+        int sum = a + b ;
+        // Substraction -
+        int sub = a - b;
+        // Multiplication *
+        int mul = a * b ;
+        // Division /
+        int div = a / b ;
+        // Modules %
+        int mod = a % b ;
+        // prefix increment
+//        ++a;
+        System.out.println(++a);
+        // prefix decrement
+//        --a;
+        System.out.println(--a);
+//        System.out.println(a);
+        // postfix incremant
+//        a++;
+        System.out.println(a++);
+        // postfix decrement
+//        a--;
+        System.out.println(a--);
+        System.out.println(a);
+    }
+
+}
+
+
