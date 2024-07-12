2e465f6a51a6432549c4a69bf793379014ab399b
Abdelhadi HADDADI <76612137+abdelhadi-haddadi@users.noreply.github.com>
2024-07-12 19:44:35 +0100

    Create Assignment_Operators.java

---
+++ Assignment_Operators.java
@@ -0,0 +1,14 @@
+public class Assignment_Operators {
+    // Assignment Operators =   x = x + 1
+    public  static  void main(String[] args){
+
+        String Name = "abdelhadi";
+        int number = 3 ;
+        number = number + 7 ;
+        number += 7 ;
+        System.out.println("The Name Is :" + Name);
+        System.out.println(number);
+
+    }
+
+}
