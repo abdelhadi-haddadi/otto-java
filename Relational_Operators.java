5bd3621044b2bb1f2fd742899812429b35658713
Abdelhadi HADDADI <76612137+abdelhadi-haddadi@users.noreply.github.com>
2024-07-12 19:48:55 +0100

    Create Relational_Operators.java

---
+++ Relational_Operators.java
@@ -0,0 +1,14 @@
+public class Relational_Operators {
+
+    public static void main(String[] args ){
+        int a = 12 , b = 100 ;
+      // != <= >= 
+//        boolean bool = a > b ;
+//        boolean bool = a == b ;
+        boolean bool = a < b ;
+
+        System.out.println(bool);
+
+
+    }
+}
