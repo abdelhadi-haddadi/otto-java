7695113e98519566057d1ee2c8976af19d62d2db
Abdelhadi HADDADI <76612137+abdelhadi-haddadi@users.noreply.github.com>
2024-07-12 21:33:35 +0100

    Update for_loop.java

--- for_loop.java
+++ for_loop.java
@@ -1,7 +1,19 @@
+import java.util.Scanner;
+
 public class for_loop {
     public static void main(String[] args){
-        for (int i = 0 ; i < 10 ; i++){
-            System.out.println("Message :" +i);
+//        for (int i = 0 ; i < 10 ; i++){
+//            System.out.println("Message :" +i);
+//        }
+        Scanner sc = new Scanner(System.in);
+        int mark ;
+        int sum = 0 ;
+        for (int count = 0 ; count < 5 ; count++) {
+            System.out.println("Enter The Mark "+ count);
+            mark = sc.nextInt();
+            sum += mark ;
+
         }
+        System.out.println("Average is : " +  (sum / 5));
     }
 }
