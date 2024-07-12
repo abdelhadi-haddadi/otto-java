2ae8450197ba51c83765ae53318d1f1f9a552ad3
Abdelhadi HADDADI <76612137+abdelhadi-haddadi@users.noreply.github.com>
2024-07-12 20:35:13 +0100

    Create Conditional_Operator_And_Break_Points_Debugging.java

---
+++ Conditional_Operator_And_Break_Points_Debugging.java
@@ -0,0 +1,15 @@
+public class Conditional_Operator_And_Break_Points_Debugging {
+    public static void main(String[] args){
+
+        String lastname = "haddadi";
+        String firstname = "abdelhadi";
+        String fullname = firstname + " " + lastname ;
+        System.out.println(fullname);
+
+        // conditional operator
+        int age = 22 ;
+        String msg = age < 20 ? "You are Young" : "You are old"  ;
+        System.out.println(msg);
+
+    }
+}
